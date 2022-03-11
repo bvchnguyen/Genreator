1ad021c30510efce2859bae2193bb103406fb003
bvchnguyen <bang4509@colorado.edu>
2022-03-10 21:27:28 -0700

    Fixed secrets

--- secrets.py
+++
@@ -1,8 +0,0 @@
-class secret_data:
-
-    def __init__(self, api_key, channel_id):
-        self.api_key = "AIzaSyAZKUW9_gAGJBhCXFMHaJkyqoDK-RBg72g"
-        self.channel_id = "UC65afEgL62PGFWXY7n6CUbA"
-
-
-
--- test_YT_client.py
+++ test_YT_client.py
@@ -1,9 +1,9 @@
 from YT_client import YT_stats
-from secrets import secret_data
+from secret_data import secrets
 import requests
 
-API = secret_data().api_key
-channel_id = secret_data().channel_id
+API = secrets().api_key
+channel_id = secrets().channel_id
 
 url = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAZKUW9_gAGJBhCXFMHaJkyqoDK-RBg72g&channelId=UC65afEgL62PGFWXY7n6CUbA&part=id&order=date&maxResults=10'
 
