bcd53776ebf01bc55c90f253be0cd1055306d5e1
bvchnguyen <bang4509@colorado.edu>
2022-03-14 22:19:37 -0600

    Testing search via spotipy. Was able to find all the songs in list returned by youtube API

--- spotify_client.py
+++ spotify_client.py
@@ -1,23 +1,64 @@
 from cgitb import reset
+from spotipy.oauth2 import SpotifyClientCredentials
+from secret_data import clientId, clientSec
+import spotipy
+import spotipy.util as util
 import requests
 import urllib.parse
 import json
 
 class SpotifyClient(object):
-    def __init__(self, api_token):
-        self.api_token = api_token
 
-    def create_playlist(self, name, description, privacy):
+    def test_spotipy(self):
+
+        birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+            client_id=clientId, client_secret=clientSec
+        ))
+
+        results = spotify.artist_albums(birdy_uri, album_type='album')
+        albums = results['items']
+        while results['next']:
+            results = spotify.next(results)
+            albums.extend(results['items'])
+
+        for album in albums:
+            print(album['name'])
+    
+    # def __init__(self, api_token):
+    #     self.api_token = api_token
+
+    def test_search(self, artist_list, title_list):
+
+        # birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+        spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+            client_id=clientId, client_secret=clientSec
+        ))
+        for i in range(len(artist_list)):
+            artist = artist_list[i]
+            track_name = title_list[i]
+            results = spotify.search(q="artist:" + artist + " track:" + track_name, type="track")
+            if results == None: 
+                print("cannot find " + artist + track_name)
+            else:
+                print("found " + artist + track_name)  
+
+    def create_playlist(self, name, description):
+        # Function to create a playlist based on user's input
+        # Get the name and the description in main
+        # Add them to json data
         data = json.dumps({
             "name": name,
-            "description": "description",
-            "public": privacy
+            "description": description
         })
+        # API url
         url = f"https://api.spotify.com/v1/users/{self.user_id}/playlists"
         response = self._pl
 
     def search_song(self, artist, track):
+        # Function to search the song by the artist and name
         query = urllib.parse.quote (f'{artist} {track}')
+        # https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20
         url_query = f'https://api.spotify.com/v1/search?q={query}&type=track'
         response = requests.get(
             url_query,
