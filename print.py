a6ceac7c1c890e9bc5eeb5b72ed4b6e320736f20
bvchnguyen <bang4509@colorado.edu>
2022-06-08 22:18:53 -0600

    Split up welcome and menu printing

--- print.py
+++ print.py
@@ -2,9 +2,12 @@ class genreator_print(object):
     def __init__(self) -> None:
         pass
 
-    def print_menu(menuItem, left_w, right_w):
+    def print_welcome(left_w, right_w):
         print('WELCOME TO GENREATOR'.center(left_w + right_w, '-'), "\n")
         print("Genreator is a program that allows you\nto transfer a youtube playlist or video\nfrom a channel to your spotify playlist.\n")
+        print('-'.center(left_w + right_w, '-'))
+
+    def print_menu(menuItem, left_w, right_w):
         print('MENU'.center(left_w + right_w, '-'))
         for k, v in menuItem.items():
             print(k.ljust(left_w, '.') +str(v).rjust(right_w))
