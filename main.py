fdff3b7c0abb940ff551d9c33e0f22de9405b8ae
bvchnguyen <bang4509@colorado.edu>
2022-06-09 17:04:03 -0600

    Revised main menu for more responsive input validation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from YT_client import YT_stats
 from secret_data import clientId, clientSec, YT_API_KEY, channelID
 from spotify_client import SpotifyClient
 from print import genreator_print
-from helper import inputValidation
+from helper_func import inputValidation
 
 
 # Function to validate user's input regarding if they want their playlist public (or not)
@@ -30,32 +30,31 @@ def main():
     while (menu_input not in menuItem.values()):
         menu_input= input("Enter the number of the option here: ")
         if menu_input in menuItem.values():        
-            break
+            if (menu_input == '01'):
+                pass
+                # print description
+
+            elif (menu_input == '02'):
+                # print('Transfering from Youtube...')
+                channel_id = input("Enter the channel ID: ")
+                yt.get_channel_id(channel_id)
+
+            elif(menu_input == '03'):
+                # Get all the necessary input for create playlist parameters
+                playlistName = input('Name your playlist: ')
+                playlistDesc = input('Describe your playlist: ')
+                publicValidation = False
+                returnedInput = inputValidation.public_input_validation()
+                if returnedInput == 'Y':
+                    publicValidation = True
+
+                spfy.create_playlist(user_ID, playlistName, playlistDesc, publicValidation, playlistDesc)
+                print('creating a playlist...')
+            else:
+                print('Quiting program. Goodbye!')
+                break
         print('Invalid choice, please try again.')
-
     # If validation is successful, we move onto sub-menus
-    if (menu_input == '01'):
-        pass
-        # print description
-
-    elif (menu_input == '02'):
-        # print('Transfering from Youtube...')
-        channel_id = input("Enter the channel ID: ")
-        yt.get_channel_id(channel_id)
-
-    elif(menu_input == '03'):
-        # Get all the necessary input for create playlist parameters
-        playlistName = input('Name your playlist: ')
-        playlistDesc = input('Describe your playlist: ')
-        publicValidation = False
-        inputValidation.public_input_validation()
-        if inputValidation.public_input_validation() == 'Y':
-            publicValidation = True
-
-        spfy.create_playlist(user_ID, playlistName, playlistDesc, publicValidation, playlistDesc)
-        print('creating a playlist...')
-    else:
-        print('Quiting program. Goodbye!')
 
 if __name__ == '__main__':
     main()
