e4d3e2877f336da0dc990b38d7a1729f2985377e
bvchnguyen <bang4509@colorado.edu>
2022-06-12 23:21:03 -0600

    Updated readme

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 A python script that extracts a playlist or a series of videos from a youtube channel, search for each song in spotify
 and add them to a playlist either by genre or by user's choice.
 
-### Technologies:
-Python
+## Technologies & Documentation:
+* Python
 
-Youtube Data API
+* Youtube Data API - https://developers.google.com/youtube/v3
 
-Spotify API
+* Spotify API - https://developer.spotify.com/documentation/web-api/
 
-Spotipy
+* Spotipy - https://spotipy.readthedocs.io/en/2.19.0/
 
-### Requirements
+## Requirements:
 You will need to a developer's account with Youtube/google and Spotify to obtain API credentials.
 For spotify, you will be looking for a Client_ID and a Client_Secret code. Store these in a secrets file.
 
@@ -21,7 +21,7 @@ DATA API. Obtaining an API key, you then want to store this in the same secrets 
 
 MAKE SURE YOU ADD THIS FILE TO YOUR .gitignore to prevent it from being push to your git repository.
 
-### Instructions
+## Instructions:
 
 To obtain a channel ID, go to a page that you would like, for demonstration purposes, I will choose Chelsea Cutler's youtube page.
 
@@ -32,7 +32,7 @@ To obtain a channel ID, go to a page that you would like, for demonstration purp
 * The program will then search through spotify and add it to an existing playlist
 * NOTE that a lot of YouTube's music are bootlegs, unofficial, or some artist are not on spotify yet. This means that the search is not perfect. I would recommend trying to find a playlist or channel that uploads official music.
 
-### Goals (for future implementation)
+## Goals (for future implementation):
 * Finish create a playlist option
 * Add generated songs to that particular playlist
 * Implement a more streamline channel search (instead of having users manually search for the ID or convert to an ID)
--- test_YT_client.py
+++ test_YT_client.py
@@ -1,17 +1,67 @@
+from pprint import pp, pprint
+from turtle import title
 from YT_client import YT_stats
 from secret_data import clientId, clientSec, YT_API_KEY, channelID
 from spotify_client import SpotifyClient
 import requests
 
-sp = SpotifyClient()
+spfy = SpotifyClient(clientId, clientSec)
 yt = YT_stats(YT_API_KEY, channelID)
 
-yt.get_channel_stats()
-title_list = yt.get_channel_video_title()
-artist, track = yt.split_title(title_list)
-sp.search_spotify(artist, track)
-# yt.dump()
+def test_YT():
+    yt.get_channel_stats()
+    yt.get_channel_name()
+    # title_list = yt.get_channel_video_title()
+    # filtered_title = yt.filter_name(title_list)
+    # # artist, track = yt.split_title(title_list)
 
+    # # print(title_list)
+    # pprint(filtered_title)
+    # sp.search_spotify(artist, track)
+
+    # yt.dump()
+
+def main():
+    channelList = ['Besomorph, 2Scratch & M.I.M.E - Blessed ',
+                    'DaWave & Monkid - Energy',
+                    'Hensonn - Sahara ',
+                    'Ivy Lab - Double Blind',
+                    'Layto - S.O.A.G.',
+                    'Tommygunnz x Brxly x N.E.B. - Too Much',
+                    'CryJaxx - Lollipop ',
+                    'Tiësto & Ava Max - The Motto ',
+                    'YehMe2 - Dog Eat Dog ']
+
+    title_list = ['Yellow Claw presents €URO TRA$H - The Function Feat. Sky Sky (DYSOMIA Remix)',
+            'Boombox Cartel - Heart Of Stone (ft. Nevve)',
+            'Just A Gent - but i wish',
+            'Ricky Remedy - Sometimes (ft.  TITUS)',
+            'vowl. - headlock',
+            'LISA - MONEY (CBznar Remix)',
+            'Adele - Easy On Me (Wazad & Alieah Cover Remix)',
+            'Kanye West - Jonah (Juelz Remix)',
+            'Trippie Redd - Miss The Rage ft. Playboi Carti (Pozy Remix)',
+            'Hermitude - HyperParadise (Tropkillaz Remix)',
+            ]
+
+    
+    test_YT()
+    # yt.get_channel_name()
+
+    # filtered_title = yt.filter_name(title_list)
+    # artist, track = yt.split_title(filtered_title)
+
+    # pprint(filtered_title)
+    # pprint(artist)
+    # pprint(track)
+
+    # print('Tracks found:', spfy.search_track_spotify(artist, track))
+
+    spotify_username = 'fdjahklfdalk'
+    # spfy.user_validation(spotify_username)
+
+if __name__ == '__main__':
+    main()
 
 
                 
