e13e19ea4c8a8d5b26f6af7089d2e17f7c7f795d
bvchnguyen <bang4509@colorado.edu>
2022-06-12 23:19:56 -0600

    Cleaned up validations

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def main():
     genreator_print.print_welcome(31,8)
 
     user_ID = input('Enter your spotify username to proceed: ')
-
-    spfy = SpotifyClient(clientId, clientSec, user_ID)
+    spfy = SpotifyClient(clientId, clientSec)
+    spfy.user_validation(user_ID)
 
     genreator_print.print_menu(menuItem, 31,8)
 
     # Validating the menu input
     menu_input = ''
     while (menu_input not in menuItem.values()):
-        menu_input= input("Enter the number of the option here: ")
+        menu_input= input('Enter the number of the option here: ')
         if menu_input in menuItem.values():        
             if (menu_input == '01'):
                 pass
@@ -34,8 +34,9 @@ def main():
 
             elif (menu_input == '02'):
                 # print('Transfering from Youtube...')
-                channel_id = input("Enter the channel ID: ")
+                channel_id = input('Enter the channel ID: ')
 
+                # Encapsulate this into a function
                 yt = YT_stats(YT_API_KEY, channel_id)
                 yt.get_channel_id(channel_id)
                 yt.get_channel_stats()
@@ -43,8 +44,7 @@ def main():
                 print('Extracting song info from', channel_name)
                 title_list = yt.get_channel_video_title()
                 filtered_title = yt.filter_name(title_list)
-                genreator_print.print_list(filtered_title, channel_name, 31, 8)
-                
+                genreator_print.print_list(filtered_title, channel_name, 31, 8)                
                 createPlaylist_YN = input('Make a new playlist to store the returned items?(Y/N): ')
                 
                 if inputValidation.YN_validation(createPlaylist_YN) == True:
@@ -58,8 +58,8 @@ def main():
             else:
                 print('Quiting program. Goodbye!')
                 break
-        print('Invalid choice, please try again.')
-    # If validation is successful, we move onto sub-menus
+        else:
+            print('Invalid choice, please try again.')
 
 if __name__ == '__main__':
     main()
